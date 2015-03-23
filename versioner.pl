964b631745c4f649e5b6671abcdabeb24d3d6f08
Jaggy Gauran <jaggygauran@gmail.com>
2015-03-23 23:42:56 +0800

    Add documentation 'cause I'm lost without it.

--- versioner.pl
+++ versioner.pl
@@ -1,7 +1,17 @@
 #!/usr/bin/perl
 
+# Get the version type. (major, minor, patch)
+
+# Get the git status.
 my $status = `git status`;
+
+# Match all the modified files.
 my @files  = ($status =~ m/modified:\s+(.+)/g);
 
+# Loop through the files.
 foreach $file (@files) {
+
+    # Look for the current version number.
+    # Update the file depending on the version type.
+    # Do a find an replace on the file.
 }
