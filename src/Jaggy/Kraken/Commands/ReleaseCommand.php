16a92150b73f1ce23060a0a42dd94aad0465ba4f
Jaggy Gauran <jaggygauran@gmail.com>
2015-03-24 15:00:43 +0800

    Use the symfony console.

---
+++ src/Jaggy/Kraken/Commands/ReleaseCommand.php
@@ -0,0 +1,63 @@
+<?php
+namespace Jaggy\Kraken\Commands;
+
+use Jaggy\Kraken\Kraken;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+
+/**
+ * Release the Kraken!
+ *
+ * @package     Jaggy\Kraken\Release
+ * @author      Jaggy Gauran <jaggygauran@gmail.com>
+ * @license     http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @version     Release: 0.1.0
+ * @link        http://github.com/jaggy/kraken
+ * @since       Class available since Release 0.1.0
+ */
+class ReleaseCommand extends Command
+{
+    /**
+     * Create a command instance.
+     *
+     * @return ReleaseCommand
+     */
+    public function __construct(Kraken $kraken)
+    {
+        parent::__construct();
+
+        $this->kraken = $kraken;
+    }
+
+    /**
+     * Configure
+     *
+     * @return void
+     */
+    public function configure()
+    {
+        $this->setName('release');
+        $this->setDescription('Update all the modified file versions.');
+
+        $this->addArgument('type', InputArgument::REQUIRED);
+
+    }
+
+
+    /**
+     * Execute the command.
+     *
+     * @return <`5:void`>
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $type = $input->getArgument('type');
+
+        $kraken = call_user_func_array([$this->kraken, $type], []);
+        $kraken->update();
+    }
+}
